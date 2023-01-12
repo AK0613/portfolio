24b00232436f0f1e5e3345ed6d1bfb322b288309
Albert Montufar <amontufar@madcapsoftware.com>
2023-01-12 12:20:33 -0800

    Made more changes

--- server.py
+++ server.py
@@ -1,20 +1,24 @@
-from flask import Flask, render_template, send_from_directory, request, redirect, url_for
+from flask import Flask, render_template, request, redirect
 import csv
 
+# Flags to enable the debug mode so I can make changes without having to restart the flask server.
 app = Flask(__name__)
 app.debug = True
 
 
+# Landing page route
 @app.route('/')
 def my_home():
     return render_template('index.html')
 
 
+# Generic handler for extra pages that may be added in the future
 @app.route('/<string:page_name>')
 def html_page(page_name):
     return render_template(page_name)
 
 
+# When a form is submitted, this appends the information into a text file
 def write_to_file(data):
     with open('database.txt', 'a') as file:
         name = data.get('name')
@@ -25,6 +29,7 @@ def write_to_file(data):
                    f'Message: {message}\n')
 
 
+# Writes form content into a CSV file
 def write_to_csv(data):
     with open('database.csv', mode='a', newline='') as database:
         name = data.get('name')
@@ -34,6 +39,8 @@ def write_to_csv(data):
         csv_writer.writerow([name, email, message])
 
 
+# Form submission handler. Uses POST request for the information. Then, all the data is stored in a dictionary for later use
+# Redirects to a Thank You page!
 @app.route('/submit_form', methods=['POST', 'GET'])
 def submit_form():
     if request.method == 'POST':
@@ -50,9 +57,10 @@ def submit_form():
 if __name__ == "__main__":
     app.run(port=5000, debug=True, use_reloader=True)
 
-    # Run in terminal
-    # set FLASK_APP=server.py
-    # $env:FLASK_APP = "server.py"
-    # python -m flask run
+# Commmands needed for the terminal
+# Run in terminal
+# set FLASK_APP=server.py
+# $env:FLASK_APP = "server.py"
+# python -m flask run
 
-    # If set FLASK_ENV=development enables the debug mode
+# If set FLASK_ENV=development enables the debug mode
